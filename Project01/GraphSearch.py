53d320a23ff88e09f17a0a98475d67aa1bf6ebf3
ktranfaglia1 <kyletranfaglia@gmail.com>
2024-10-10 13:24:54 -0400

    Finished Project01 code

--- Project01/GraphSearch.py
+++ Project01/GraphSearch.py
@@ -6,8 +6,9 @@
 
 import time
 from collections import deque
+from queue import PriorityQueue
 
-# Undirected city graph with distances (miles) in alphabetical order with shortest to longest paths (edges)
+# Undirected city graph with distances (miles) in alphabetical order with shortest to the longest paths (edges)
 city_graph = {
     'Baltimore': {'Washington DC': 45, 'Salisbury': 117, 'Philadelphia': 101, 'Pittsburgh': 248},
     'Boston': {'New York': 216, 'Buffalo': 450},
@@ -26,24 +27,28 @@ city_graph = {
 def bfs(graph, start, destination):
     start_time = time.perf_counter()  # Start timer
 
-    # Queue (doubled-ended) for BFS (current city, path so far, total distance traveled)
+    # Queue (doubled-ended | FIFO) for BFS that stores a tuple: (current city, path so far, total distance traveled)
     queue = deque([(start, [start], 0)])
-    # Set to track visited cities and prevent revisits & infinite loops
-    visited = set()
+    # List to track visited cities and prevent revisits & infinite loops
+    visited = []
 
     # Continue search as long as there are cities in the queue left to explore
     while (queue):
-        city, path, distance = queue.popleft()  # Remove first city from queue (FIFO) and store the tuple individually
+        # Remove first city from queue (FIFO) and store the tuple individually | O(1) compared to O(n) for .pop(0)
+        city, path, distance = queue.popleft()
 
+        # Check if destination has been reached
         if (city == destination):
             # End timer and calculate time taken to complete search
             end_time = time.perf_counter()
             time_taken = end_time - start_time
 
             # Print results from BFS
-            print(f"\nPath: {' -> '.join(path)}")  # join list elements of path into a single string separated by ->
+            print("\nBFS Path:")
+            print(f"Path: {' -> '.join(path)}")  # join list elements of path into a single string separated by ->
             print(f"Total distance: {distance} miles")
             print(f"Time taken: {time_taken:.7f} seconds")
+
             return
         else:
             # Explore all neighboring cities if not already visited
@@ -52,7 +57,93 @@ def bfs(graph, start, destination):
                 if (neighbor not in visited):
                     queue.append((neighbor, path + [neighbor], distance + miles))
 
-            visited.add(city)  # Add current city to set to avoid revisit
+            visited.append(city)  # Add current city to set to avoid revisit
+
+    # Since the graph is connected, this should never happen with valid input: displays message on a failed search
+    print(f"No path found from {start} to {destination}")
+
+
+# Depth First Search algorithm to finds a path (leftmost | first found) between a start and end city
+def dfs(graph, start, destination):
+    start_time = time.perf_counter()  # Start timer
+
+    # Stack (LIFO) for DFS that stores a tuple: (current city, path so far, total distance traveled)
+    stack = [(start, [start], 0)]
+    # List to track visited cities and prevent revisits & infinite loops
+    visited = []
+
+    # Continue search as long as there are cities in the stack left to explore
+    while (stack):
+        # Remove first city from queue (FIFO) and store the tuple individually | O(1) compared to O(n) for .pop(0)
+        city, path, distance = stack.pop()
+
+        # Check if destination has been reached
+        if (city == destination):
+            # End timer and calculate time taken to complete search
+            end_time = time.perf_counter()
+            time_taken = end_time - start_time
+
+            # Print results from DFS
+            print("\nDFS Path:")
+            print(f"Path: {' -> '.join(path)}")  # join list elements of path into a single string separated by ->
+            print(f"Total distance: {distance} miles")
+            print(f"Time taken: {time_taken:.7f} seconds")
+
+            return
+        else:
+            # Explore all neighboring cities if not already visited
+            for neighbor, miles in graph[city].items():
+                # Check if a neighbor has been visited, then add it to the stack with an updated path and total distance
+                if (neighbor not in visited):
+                    stack.append((neighbor, path + [neighbor], distance + miles))
+
+            visited.append(city)  # Add current city to set to avoid revisit
+
+    # Since the graph is connected, this should never happen with valid input: displays message on a failed search
+    print(f"No path found from {start} to {destination}")
+
+
+# Uniform Cost Search algorithm to finds the optimal (the least cost) path between a start and end city
+def ucs(graph, start, destination):
+    start_time = time.perf_counter()  # Start timer
+
+    # Priority queue for UCS that stores a tuple: (total distance traveled *priority item*, current city, path so far)
+    priority_queue = PriorityQueue()
+    priority_queue.put((0, start, [start]))  # Initialize with the start city and distance as 0 (distance is priority)
+
+    # Dictionary to track the least cost to reach each city
+    visited = {}
+
+    # Continue search as long as there are cities in the priority queue left to explore
+    while (not priority_queue.empty()):
+        # Get the city with the lowest cost from the priority queue and store the tuple individually
+        distance, city, path = priority_queue.get()
+
+        # Check if destination has been reached
+        if (city == destination):
+            # End timer and calculate time taken to complete search
+            end_time = time.perf_counter()
+            time_taken = end_time - start_time
+
+            # Print results from DFS
+            print("\nUCS Path:")
+            print(f"Path: {' -> '.join(path)}")  # join list elements of path into a single string separated by ->
+            print(f"Total distance: {distance} miles")
+            print(f"Time taken: {time_taken:.7f} seconds")
+
+            return
+        else:
+            # Visit the city if not yet visited or revisit if a smaller distance (ensure optimality) is found
+            if (city not in visited or distance < visited[city]):
+                visited[city] = distance  # Set the minimum cost to reach this city
+
+                # Explore all neighboring cities
+                for neighbor, miles in graph[city].items():
+                    new_distance = distance + miles  # Calculate the new total distance (cost) to reach the neighbor
+
+                    # Check if it's a new city or has a lower cost and add the neighbor to the priority queue
+                    if (neighbor not in visited or new_distance < visited[neighbor]):
+                        priority_queue.put((new_distance, neighbor, path + [neighbor]))
 
     # Since the graph is connected, this should never happen with valid input: displays message on a failed search
     print(f"No path found from {start} to {destination}")
@@ -70,8 +161,8 @@ def get_city_input(prompt, graph):
     return city
 
 
-# Driver program
-if __name__ == "__main__":
+# Driver program (main)
+if (__name__ == "__main__"):
     # Program welcome message
     print("Find a path from city x to city y using the following: Baltimore, "
           "Boston, Buffalo, New York, Norfolk, Philadelphia, Pittsburgh, Richmond, Salisbury, Washington DC")
@@ -82,3 +173,9 @@ if __name__ == "__main__":
 
     # Call BFS function to find & display the shortest path (the least cities traveled)
     bfs(city_graph, start_city, destination_city)
+
+    # Call DFS function to find & display a path (leftmost | first found)
+    dfs(city_graph, start_city, destination_city)
+
+    # Call UCS function to find & display the optimal path (the lowest cost/distance)
+    ucs(city_graph, start_city, destination_city)
